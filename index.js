0288318a23a0633879bcc909b435766df3253dd0
azero27 <149345309+azero27@users.noreply.github.com>
2023-11-02 03:23:15 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+var express = require('express');
+var router = express.Router();
+
+//GET home page.
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+//express 모듈 불러오기
+const express = require("express");
+//express 사용
+const app = express(); 
+
+
+ * @path http://localhost:3000/ 경로
+ 
+app.get("/", (req, res) => {
+    res.send("서버실행 완료!");
+});
+  
+// http listen port 생성 서버 실행
+app.listen(3000, () => console.log("야호"));
+*/
